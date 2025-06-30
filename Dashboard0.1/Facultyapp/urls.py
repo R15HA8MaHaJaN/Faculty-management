65dc97ff1b3a55983117bcceb3238e172866d538
R15HA8MaHaJaN <rishab4441mahajan@gmail.com>
2025-06-30 22:03:29 +0200

    Deleated old project, no more needed

--- Dashboard0.1/Facultyapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'Facultyapp'
-
-
-urlpatterns = [
-    path('faculty/', views.facultypage, name='faculty'),
-]
--- Dashboard0.1/Facultyapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def facultypage(request):
-    return render(request, 'faculty.html')
